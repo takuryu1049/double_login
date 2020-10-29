260bcd4d99d64388ac033658e3c5bd1d24c56fd3
takur-ryu <takuryu.1049@icloud.com>
2020-10-29 12:33:07 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :workers
-  devise_for :companies
+  get 'home/index'
+  devise_for :workers, controllers: {
+    sessions:      'workers/sessions',
+    passwords:     'workers/passwords',
+    registrations: 'workers/registrations'
+  }
+  devise_for :companies, controllers: {
+    sessions:      'companies/sessions',
+    passwords:     'companies/passwords',
+    registrations: 'companies/registrations'
+  }
+  get 'home/index'
+  get '/top', to: 'home#index_home'
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
